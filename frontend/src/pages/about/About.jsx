685791a4897bf3a0af5ca74be4be2b808517c47d
cauelugli <cauec4@gmail.com>
2022-04-23 18:50:33 -0300

    about page tweak

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 
 import { CardContent, Box, Typography } from "@mui/material";
 
@@ -10,22 +10,23 @@ export default function About() {
       <div>
         <Box
           sx={{
-            display: 'block',
-            color: 'grey.800',
-            border: '1px solid',
-            margin: '2%',
-            borderColor: 'grey.300',
+            display: "block",
+            color: "grey.800",
+            border: "1px solid",
+            margin: "2%",
+            borderColor: "grey.400",
             borderRadius: 2,
-            fontWeight: '700',
+            fontWeight: "700",
           }}
         >
           <CardContent>
-              <Typography sx={{
-                fontStyle: 'italic',
-                color: "text.secondary",
+            <Typography
+              sx={{
+                fontStyle: "italic",
+                color: "grey.600",
               }}
-              >
-                "Pescador de ilusões. Todos nós somos."
+            >
+              "Pescador de ilusões. Todos nós somos."
             </Typography>
             <Box
               sx={{
@@ -33,23 +34,40 @@ export default function About() {
                 alignItems: "center",
                 justifyContent: "space-around",
                 textAlign: "justify",
-                margin: "2%"
+                margin: "2%",
               }}
             >
-              <img style={{ width: "30%" }} src={window.location.origin + '/adventurer.png'} alt="adventurer" />
+              <img
+                style={{ width: "25%" }}
+                src={window.location.origin + "/adventurer.png"}
+                alt="adventurer"
+              />
 
-              <Typography sx={{ padding: '5%', lineHeight: 1.8, fontSize: '150%' }}>
-                Porque a ideia é ser feito por ele, e nessa frase mora todo o propósito disto aqui.
-                "Por que a ideia...": tem que virar a ação.
-                "...é ser feito...": pois há o fato de fazer por fazer.
-                "por ele.": quem é ele?
-                <strong> Caue Lugli dos Milagres,</strong> ou simplesmente <strong>Caue, pokamassa, c4aum, Cau, Picuruta,</strong> você quem escolhe...
-                Eu sou teu espelho. Eu sirvo pra te mostrar um reflexo dos teus próprios pensamentos.
-                Pois assim tu me ensinas a enxergar a vida. E eu te retribuo, meu parceiro.
-                Muitas coisas vão fazer sentido, mas nem todas.
-                O importante é que estaremos juntos. No final de tudo, rindo do que aconteceu.
-                Muita energia!
-              </Typography>
+              <div id="text">
+
+                <Typography sx={{ margin: "3%", lineHeight: 1.8, fontSize: "125%" }}>
+                  Porque a ideia é ser feito por ele, e nessa frase mora todo o propósito disto aqui. Mas afinal, quem é ele?
+                </Typography>
+
+                <Typography sx={{ margin: "3%", lineHeight: 1.8, fontSize: "125%" }}>
+                  <strong> Caue Lugli dos Milagres,</strong> ou simplesmente <strong>Caue, pokamassa, c4aum, Cau, Picuruta,</strong> você
+                  quem escolhe... Eu sou teu espelho. Eu sirvo pra te mostrar um reflexo dos teus próprios pensamentos.  
+                </Typography>
+
+                <Typography sx={{ margin: "3%", lineHeight: 1.8, fontSize: "125%" }}>
+                  Pois assim tu me ensinas a enxergar a vida. E eu te retribuo, meu parceiro. Muitas coisas farão sentido, mas nem todas.
+                </Typography>
+
+                <Typography sx={{ margin: "3%", lineHeight: 1.8, fontSize: "125%" }}>
+                  O importante é que estaremos juntos no final de tudo, e rindo do que aconteceu.
+                </Typography>
+
+                <Typography sx={{ margin: "3%", lineHeight: 1.8, fontSize: "125%" }}>
+                  Muita energia!
+                </Typography>
+
+              </div>
+
             </Box>
           </CardContent>
         </Box>
