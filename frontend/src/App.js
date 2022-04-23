a027af3cace9d3325ffc2e74ded8d216ad2c14b3
cauelugli <cauec4@gmail.com>
2022-04-23 15:22:32 -0300

    fix singlePost page component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,8 +8,10 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import About from "./pages/about/About";
 
+
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import SinglePost from "./components/singlePost/SinglePost";
 
 function App() {
   const { user } = useContext(Context);
@@ -24,8 +26,7 @@ function App() {
         <Route path="/login">{user ? <Home /> : <Login />}</Route>
         <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
         <Route path="/about">{user ? <About /> : <About />}</Route>
-        <Route path="/post/:postId">
-        </Route>
+        <Route path="/post/:postId" ><SinglePost /> </Route>
       </Switch>
       
     <Footer />
