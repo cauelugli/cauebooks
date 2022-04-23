5ffa93e17a65b952bf2221531a26011c2e1b7007
cauelugli <cauec4@gmail.com>
2022-04-23 18:29:31 -0300

    settings menu added icons and padding

--- frontend/src/components/settingsMenu/SettingsMenu.jsx
+++ frontend/src/components/settingsMenu/SettingsMenu.jsx
@@ -1,13 +1,15 @@
-import * as React from "react";
-import { useContext } from "react";
+import React, { useState, useContext } from "react";
 import { Link } from "react-router-dom";
 
 import { IconButton, Menu, MenuItem } from "@mui/material";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import MeetingRoomOutlinedIcon from '@mui/icons-material/MeetingRoomOutlined';
 
 import { Context } from "../../context/Context";
 
 export default function BasicMenu() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   // eslint-disable-next-line
   const { user, dispatch } = useContext(Context);
@@ -21,23 +23,37 @@ export default function BasicMenu() {
 
   return (
     <div>
-      <IconButton sx={{ "&:hover": { backgroundColor: "transparent" }} } onClick={handleClick}>
+      <IconButton
+        sx={{ "&:hover": { backgroundColor: "transparent" } }}
+        onClick={handleClick}
+      >
         <img
           className="topImg"
           src={window.location.origin + "/" + user.avatar + ".png"}
           alt=""
         />
       </IconButton>
-      <Menu
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose}>
-          <Link to="/settings">Settings</Link>
+
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+
+        {/* <MenuItem onClick={handleClose}> */}
+        <MenuItem onClick={() => alert('one day')}>
+          <FavoriteBorderOutlinedIcon style={{ marginRight: "10%" }} />
+          <p style={{ marginRight: "5px" }}>Favoritos</p>
         </MenuItem>
-        <MenuItem onClick={handleClose}>My account</MenuItem>
-        <MenuItem onClick={handleClose}>Logout</MenuItem>
+
+        <Link className="link" to="/settings">
+          <MenuItem onClick={handleClose}>
+            <AccountCircleOutlinedIcon style={{ marginRight: "10%" }} />
+            <p style={{ marginRight: "5px" }}>Conta</p>
+          </MenuItem>
+        </Link>
+
+        {/* <MenuItem onClick={handleClose}> */}
+        <MenuItem onClick={() => alert('one day')}>
+          <MeetingRoomOutlinedIcon style={{ marginRight: "10%" }} />
+          <p style={{ marginRight: "5px" }}>Logout</p>
+        </MenuItem>
       </Menu>
     </div>
   );
