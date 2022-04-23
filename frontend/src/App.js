3fee4a73c0d11ccccacec52795e794ce7b2cac2c
cauelugli <cauec4@gmail.com>
2022-04-23 18:30:17 -0300

    singlePost component is now a page (directory move, it's still a component tho...)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,26 +7,24 @@ import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import About from "./pages/about/About";
-
-
+import SinglePost from "./pages/singlePost/SinglePost";
 
 import Header from "./components/header/Header";
-import SinglePost from "./components/singlePost/SinglePost";
 
 function App() {
   const { user } = useContext(Context);
   return (
     <>
     <Router>
-    <Header />
+    {user && <Header />}
 
       <Switch>
-        <Route exact path="/"><Home /></Route>
-        <Route path="/register">{user ? <Home /> : <Register />}</Route>
-        <Route path="/login">{user ? <Home /> : <Login />}</Route>
-        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
-        <Route path="/about">{user ? <About /> : <About />}</Route>
-        <Route path="/post/:postId" ><SinglePost /> </Route>
+        <Route exact path="/">{user ? <Home /> : <Login />}</Route>
+        <Route path="/login"><Login /></Route>
+        <Route path="/register"><Register /></Route>
+        <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
+        <Route path="/about">{user ? <About /> : <Login />}</Route>
+        <Route path="/post/:postId" >{user ? <SinglePost /> : <Login />} </Route>
       </Switch>
      
     </Router>
