16054a4606adfa3d89f42322831a7a2650b4023a
cauelugli <cauec4@gmail.com>
2022-04-24 18:22:55 -0300

    romved delete post router

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,5 +1,4 @@
 const router = require("express").Router();
-const User = require("../models/User");
 const Post = require("../models/Post");
 
 //CREATE POST
@@ -38,24 +37,6 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//DELETE POST
-router.delete("/:id", async (req, res) => {
-  try {
-    const post = await Post.findById(req.params.id);
-    if (post.username === req.body.username) {
-      try {
-        await post.delete();
-        res.status(200).json("Post has been deleted...");
-      } catch (err) {
-        res.status(500).json(err);
-      }
-    } else {
-      res.status(401).json("You can delete only your post!");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
 
 //GET POST
 router.get("/:id", async (req, res) => {
