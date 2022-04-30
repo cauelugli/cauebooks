490c70cd16be99c6958eaaaa6d8a0082df773902
cauelugli <cauec4@gmail.com>
2022-04-30 13:13:10 -0300

    code refactoring

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,19 +1,10 @@
-import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Context } from "../../context/Context";
-import CategoriesBar from "../categoriesBar/CategoriesBar";
 
+import CategoriesBar from "../categoriesBar/CategoriesBar";
 import SearchBar from "../searchbar/SearchBar";
 import SettingsMenu from "../settingsMenu/SettingsMenu";
 
 export default function Header({ data }) {
-  // eslint-disable-next-line
-  const { user, dispatch } = useContext(Context);
-
-  // const handleLogout = () => {
-  //   dispatch({ type: "LOGOUT" });
-  // };
-
   return (
     <>
       <div className="header">
