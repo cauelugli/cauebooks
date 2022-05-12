1e8137c9ed5eb8201bc7a3cd191f9cc23f5ef974
cauelugli <cauec4@gmail.com>
2022-05-11 21:09:57 -0300

    preparing post for like action

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,18 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
 
-//CREATE POST
-router.post("/", async (req, res) => {
-  const newPost = new Post(req.body);
-  try {
-    const savedPost = await newPost.save();
-    res.status(200).json(savedPost);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});
-
 //GET POST
 router.get("/:id", async (req, res) => {
   try {
@@ -41,6 +29,39 @@ router.get("/", async (req, res) => {
       posts = await Post.find();
     }
     res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//CREATE POST
+router.post("/", async (req, res) => {
+  const newPost = new Post(req.body);
+  try {
+    const savedPost = await newPost.save();
+    res.status(200).json(savedPost);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+//UPDATE POST
+router.put("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    try {
+      const updatedPost = await Post.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   } catch (err) {
     res.status(500).json(err);
   }
--- frontend/src/pages/singlePost/SinglePost.jsx
+++ frontend/src/pages/singlePost/SinglePost.jsx
@@ -3,7 +3,9 @@ import { useLocation } from "react-router";
 
 import axios from "axios";
 
-import { Box, Paper, Typography } from "@mui/material";
+import { Box, IconButton, Paper, Typography } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 import { Context } from "../../context/Context";
 
@@ -15,6 +17,8 @@ export default function SinglePost() {
   const [title, setTitle] = useState("");
   const [categories, setCategories] = useState("");
   const [body, setBody] = useState("");
+  const [likes, setLikes] = useState(0);
+  const [favorite, setFavorite] = useState(false);
 
   useEffect(() => {
     const getPost = async () => {
@@ -22,29 +26,65 @@ export default function SinglePost() {
       setTitle(res.data.title);
       setCategories(res.data.categories);
       setBody(res.data.body);
+      setLikes(res.data.likes);
+      console.log(res.config.url);
     };
     getPost();
   }, [path]);
 
+  // const handleUpdate = async () => {
+  //   try {
+  //     await axios.put(`/posts/${id}`, {
+  //       likes: + 1,
+  //     });
+  //     alert("You liked the post!")
+  //   } catch (err) {
+  //     alert("You removed your like!")
+  //   }
+  // };
+
+  const handleFavorite = () => {
+    if (!favorite) {
+      // axios.post
+      setFavorite(true);
+      // this is fake
+      setLikes(likes + 1);
+    } else {
+      // axios.post
+      setFavorite(false);
+      // this is fake
+      setLikes(likes - 1);
+    }
+  };
 
   return (
     <div className="singlePost">
-      <Box className="singlePostBox"
+      <Box
+        className="singlePostBox"
         sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
+          display: "flex",
+          flexWrap: "wrap",
+          "& > :not(style)": {
             m: 1,
-            width: '100%',
+            width: "100%",
           },
-        }}>
+        }}
+      >
         <Paper elevation={3}>
-          <Typography variant="h3" className="singlePostTitle">{title}</Typography>
-          <Typography variant="h6" className="singlePostTitle">{categories}</Typography>
+          <Typography variant="h3" className="singlePostTitle">
+            {title}
+          </Typography>
+          <Typography variant="h6" className="singlePostTitle">
+            {categories}
+          </Typography>
           <p className="singlePostDesc">{body}</p>
+          <IconButton onClick={handleFavorite} sx={{marginLeft:"92%"}}>
+            {!favorite && <FavoriteBorderIcon sx={{marginRight:"8%"}}/>}
+            {favorite && <FavoriteIcon sx={{marginRight:"8%"}}/>}
+            {likes}
+          </IconButton>
         </Paper>
       </Box>
-      
     </div>
   );
 }
