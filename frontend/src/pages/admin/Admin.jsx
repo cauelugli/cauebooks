9fa117eece19adce33b1b307342bf5044c592bc7
cauelugli <cauec4@gmail.com>
2022-06-14 20:19:36 -0300

    added Rich Text component working properly to the db, sending html as body prop

--- frontend/src/pages/admin/Admin.jsx
+++ frontend/src/pages/admin/Admin.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
+import { EditorState, convertToRaw } from "draft-js";
+import draftToHtml from "draftjs-to-html";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+
 import axios from "axios";
+import { Editor } from "react-draft-wysiwyg";
 
 import {
   InputLabel,
@@ -30,7 +35,7 @@ const categoriesList = [
 export default function Admin() {
   const [title, setTitle] = useState("");
   const [categories, setCategories] = useState([]);
-  const [body, setBody] = useState("");
+  const [state, setState] = useState({editorState: EditorState.createEmpty()});
 
   const handleCategories = (event) => {
     const {
@@ -44,15 +49,25 @@ export default function Admin() {
     try {
       const res = await axios.post("/posts", {
         title,
-        body,
+        body: parsedState,
         categories,
       });
       res.data && alert("niceru!");
     } catch (err) {
-      alert("Vish, deu não...")
+      alert("Vish, deu não...");
       console.log(err);
     }
   };
+
+  const handleEditorStateChange = (editorState) => {
+    setState({
+      editorState,
+    });
+  };
+
+  const { editorState } = state;
+
+  const parsedState = draftToHtml(convertToRaw(editorState.getCurrentContent()))
 
   return (
     <div className="admin">
@@ -93,16 +108,27 @@ export default function Admin() {
             sx={{ m: 4, width: "50%", backgroundColor: "#e4e4e4" }}
             variant="outlined"
           >
-            <InputLabel>Paragraphs</InputLabel>
-            <OutlinedInput
-              type={"text"}
-              value={body}
-              onChange={(e) => setBody(e.target.value)}
-              label="Paragraphs"
+            <Editor
+              editorState={editorState}
+              wrapperClassName="demo-wrapper"
+              editorClassName="demo-editor"
+              onEditorStateChange={handleEditorStateChange}
             />
           </FormControl>
-          <Button sx={{color: "#BDEFD8", backgroundColor:"#0E1428"}} type="submit">DALHE!</Button>
+
+          <Button
+            sx={{ color: "#BDEFD8", backgroundColor: "#0E1428" }}
+            type="submit"
+          >
+            DALHE!
+          </Button>
         </form>
+        <Button
+          sx={{ color: "#BDEFD8", backgroundColor: "#0E1428" }}
+          onClick={() => console.log(draftToHtml(convertToRaw(editorState.getCurrentContent())))}
+        >
+          SHOW ME THE TEXT!
+        </Button>
       </div>
     </div>
   );
