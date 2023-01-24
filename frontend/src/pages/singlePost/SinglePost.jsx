71f635999684f552fb5d94d7620536fec1b0bccd
cauelugli <cauec4@gmail.com>
2023-01-24 01:13:17 -0300

    singlePost page tweak, and displaying dynamic categories

--- frontend/src/pages/singlePost/SinglePost.jsx
+++ frontend/src/pages/singlePost/SinglePost.jsx
@@ -6,8 +6,8 @@ import axios from "axios";
 import { Box, CardContent, Grid, IconButton, Typography } from "@mui/material";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import StarIcon from "@mui/icons-material/Star";
 
 import { Context } from "../../context/Context";
 
@@ -27,7 +27,7 @@ export default function SinglePost() {
   useEffect(() => {
     const getPost = async () => {
       const res = await axios.get("/posts/" + postId);
-      console.log('user', user);
+
       setTitle(res.data.title);
       setCategories(res.data.categories);
       setBody(res.data.body);
@@ -35,23 +35,20 @@ export default function SinglePost() {
       setFavorites(res.data.favorites);
     };
 
-    console.log('getPost', getPost())
-
-    const res1 = () => getPost()
+    const res1 = () => getPost();
 
     const getUser = async () => {
       const realUser = await axios.get("/users/" + user._id);
-      console.log('realUser.data', realUser.data);
+
       if (realUser.data.likesList.includes(res1.title)) {
         setLikesThisPost(true);
       }
       if (realUser.data.favoritesList.includes(res1.title)) {
         setFavorite(true);
       }
-      console.log('realUser.data', realUser.data)
-    }
-    getPost();    
-    getUser();    
+    };
+    getPost();
+    getUser();
   }, [postId, user]);
 
   const handleLike = async (e) => {
@@ -61,9 +58,9 @@ export default function SinglePost() {
         await axios.put("/users/" + user._id, { likesList: title });
         await axios.put("/posts/" + postId, { likes: likes + 1 });
         setLikes(likes + 1);
-        setLikesThisPost(true)
+        setLikesThisPost(true);
         dispatch({ type: "LIKE", payload: title });
-        console.log('user LIKE', user)
+        console.log("user LIKE", user);
       } catch (err) {
         console.log(err);
       }
@@ -72,9 +69,9 @@ export default function SinglePost() {
         await axios.put("/users/" + user._id, { likesList: title });
         await axios.put("/posts/" + postId, { likes: likes - 1 });
         setLikes(likes - 1);
-        setLikesThisPost(false)
+        setLikesThisPost(false);
         dispatch({ type: "UNLIKE", payload: title });
-        console.log('user UNLIKE', user)
+        console.log("user UNLIKE", user);
       } catch (err) {
         console.log(err);
       }
@@ -90,7 +87,7 @@ export default function SinglePost() {
         setFavorites(favorites + 1);
         setFavorite(true);
         dispatch({ type: "FAVORITE", payload: title });
-        console.log('user FAVORITE', user)
+        console.log("user FAVORITE", user);
       } catch (err) {
         console.log(err);
       }
@@ -101,7 +98,7 @@ export default function SinglePost() {
         setFavorites(favorites - 1);
         setFavorite(false);
         dispatch({ type: "UNFAVORITE", payload: title });
-        console.log('user UNFAVORITE', user)
+        console.log("user UNFAVORITE", user);
       } catch (err) {
         console.log(err);
       }
@@ -110,16 +107,20 @@ export default function SinglePost() {
 
   let categoriesList = "";
   if (categories) {
-    categories.forEach(myFunction);
-
-    function myFunction(item) {
-      // try to render the components, maybe will fuck the shit up... but anyway...
-      categoriesList += item + " "; 
-    }
+    categories.forEach((item) => {
+      categoriesList += item + " ";
+    });
   }
 
   return (
-    <Box sx={{display: "flex", alignItems: "center", justifyContent: "center", my: "2%"}}>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        my: "2%",
+      }}
+    >
       <Box
         sx={{
           display: "flex",
@@ -134,43 +135,59 @@ export default function SinglePost() {
         }}
       >
         <CardContent>
-          <Box sx={{display: "flex", textAlign: "", margin: "5%"}}>
-            
+          <Box sx={{ display: "flex", textAlign: "", margin: "5%" }}>
             <div>
-
-              <Box sx={{display: "flex", alignItems: "center", justifyContent: "center"}}> 
-                <Typography sx={{fontStyle: "italic", fontSize: "30px", color: "grey.800"}}>
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Typography
+                  align="justify"
+                  sx={{
+                    fontStyle: "italic",
+                    fontSize: "36px",
+                    color: "grey.800",
+                  }}
+                >
                   {title}
                 </Typography>
               </Box>
 
-
-              <Typography sx={{fontStyle: "italic", fontSize: "125%", color: "grey.800", mb: "4%", ml:"75%"}}>
-                {categoriesList}
+              <Typography
+                align="right"
+                sx={{
+                  fontStyle: "italic",
+                  color: "grey.700",
+                  my: 4,
+                }}
+              >
+                <div dangerouslySetInnerHTML={{ __html: categoriesList }} />
               </Typography>
 
-              <Typography sx={{color: "grey.800"}}>
-              <div dangerouslySetInnerHTML={{ __html: body }} />
+              <Typography sx={{ color: "grey.800" }}>
+                <div dangerouslySetInnerHTML={{ __html: body }} />
               </Typography>
-              
             </div>
-            
           </Box>
 
           <Grid container justifyContent="flex-end">
             <IconButton onClick={handleLike}>
-                {likesThisPost && <FavoriteIcon sx={{ mx: "1%", color: "#e65940" }} />}
-                {!likesThisPost && <FavoriteBorderIcon sx={{ mx: "1%" }} />}
-                {likes}
-              </IconButton>
+              {likesThisPost && (
+                <FavoriteIcon sx={{ mx: "1%", color: "#e65940" }} />
+              )}
+              {!likesThisPost && <FavoriteBorderIcon sx={{ mx: "1%" }} />}
+              {likes}
+            </IconButton>
 
-              <IconButton onClick={handleFavorite}>
-                {!favorite && <StarBorderIcon sx={{ mx: "1%" }} />}
-                {favorite && <StarIcon sx={{ mx: "1%", color: "#f2a933" }} />}
-                {favorites}
-              </IconButton>
+            <IconButton onClick={handleFavorite}>
+              {!favorite && <StarBorderIcon sx={{ mx: "1%" }} />}
+              {favorite && <StarIcon sx={{ mx: "1%", color: "#f2a933" }} />}
+              {favorites}
+            </IconButton>
           </Grid>
-          
         </CardContent>
       </Box>
     </Box>
