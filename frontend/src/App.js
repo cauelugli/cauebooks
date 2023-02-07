22eed0c193a4d7f7f3067b89df776ca7a4e1069c
cauelugli <cauec4@gmail.com>
2023-02-07 11:02:34 -0300

    categories pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './app.css'
+import "./app.css";
 
 import { Context } from "./context/Context";
 
@@ -18,30 +18,52 @@ import Settings from "./pages/settings/Settings";
 import SinglePost from "./pages/singlePost/SinglePost";
 import UserVerification from "./pages/userVerification/UserVerification";
 import ForgotPassword from "./pages/forgotPassword/ForgotPassword";
+import Categories from "./pages/categories/Categories";
 
 function App() {
   const { user } = useContext(Context);
   return (
     <>
-    <Router>
-    {user && <Header />}
+      <Router>
+        {user && <Header />}
 
-      <Switch>
-        <Route exact path="/">{user ? <Home /> : <Login />}</Route>
-        <Route path="/login"><Login /></Route>
-        <Route path="/register"><Register /></Route>
-        <Route path="/forgotPassword"><ForgotPassword /></Route>
-        <Route path="/users/activate/:id"><UserVerification /></Route>
-        <Route path="/about">{user ? <About /> : <Login />}</Route>
-        <Route path="/:userId/favorites">{user ? <Favorites /> : <Login />}</Route>
-        <Route path="/passwordReset">{user ? <PasswordReset /> : <Login />}</Route>
-        <Route path="/post/:postId" >{user ? <SinglePost /> : <Login />} </Route>
-        <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
-        <Route exact path="/admin">{user?.isAdmin ? <Admin /> : <Login />}</Route>
-      </Switch>
-     
-    <Footer />
-    </Router>
+        <Switch>
+          <Route exact path="/">
+            {user ? <Home /> : <Login />}
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/forgotPassword">
+            <ForgotPassword />
+          </Route>
+          <Route path="/users/activate/:id">
+            <UserVerification />
+          </Route>
+          <Route path="/about">{user ? <About /> : <Login />}</Route>
+          <Route path="/:userId/favorites">
+            {user ? <Favorites /> : <Login />}
+          </Route>
+          <Route path="/passwordReset">
+            {user ? <PasswordReset /> : <Login />}
+          </Route>
+          <Route path="/post/:postId">
+            {user ? <SinglePost /> : <Login />}
+          </Route>
+          <Route path="/categories/:id">
+            {user ? <Categories /> : <Login />}
+          </Route>
+          <Route path="/settings">{user ? <Settings /> : <Login />}</Route>
+          <Route exact path="/admin">
+            {user?.isAdmin ? <Admin /> : <Login />}
+          </Route>
+        </Switch>
+
+        <Footer />
+      </Router>
     </>
   );
 }
---
+++ frontend/src/pages/categories/Categories.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+import axios from "axios";
+
+import { Box, Grid, Typography } from "@mui/material";
+
+const Categories = () => {
+  const location = useLocation();
+  const categoryId = location.pathname.split("/")[2];
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryIcon, setCategoryIcon] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const { data } = await axios.get("/posts");
+      const data2 = await axios.get("/categories");
+      console.log("data2", data.posts[0].createdAt);
+
+      const foundObject = data2.data.find(
+        (object) => object._id === categoryId
+      );
+
+      setCategoryName(foundObject);
+      const categoryIconConcat = " &#" + foundObject.icon + ";";
+      setCategoryIcon(categoryIconConcat);
+
+      setPosts(
+        data.posts.filter((post) =>
+          post.categories.some((category) => category._id === categoryId)
+        )
+      );
+    };
+    getPosts();
+    console.log("categoryIcon", categoryIcon);
+  }, []);
+
+  return (
+    <Grid
+      container
+      justifyContent="center"
+      sx={{
+        p: 5,
+        m: 5,
+        color: "#0E1428",
+        border: "3px solid",
+        backgroundColor: "#f1f1f0e3",
+        borderColor: "grey.400",
+        borderRadius: 3,
+        width: "80%",
+      }}
+    >
+      <Grid container direction="row" justifyContent="" sx={{ m: 1 }}>
+        <Typography variant="h4">Categoria</Typography>
+        <Typography variant="h4" sx={{ mx: 2 }}>
+          <div dangerouslySetInnerHTML={{ __html: categoryIcon }} />
+        </Typography>
+        <Typography variant="h4">{categoryName.name}</Typography>
+      </Grid>
+      {posts.map((post) => (
+        <Box
+          sx={{
+            p: 2,
+            m: 2,
+            mt: 4,
+            color: "#0E1428",
+            border: "3px solid",
+            backgroundColor: "#f1f1f0e3",
+            borderColor: "grey.400",
+            borderRadius: 3,
+            width: "50%",
+          }}
+        >
+          <div key={post.id}>
+            <Link
+              to={`/post/${post._id}`}
+              style={{ textDecoration: "none", color: "#2e2e2e" }}
+            >
+              <h2>{post.title}</h2>
+            </Link>
+          </div>
+        </Box>
+      ))}
+    </Grid>
+  );
+};
+
+export default Categories;
