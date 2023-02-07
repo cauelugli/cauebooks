12f305e7997c88a69e9918de6f2c44707b33d32d
cauelugli <cauec4@gmail.com>
2023-02-06 22:00:28 -0300

    categoriesBar adjustment

--- frontend/src/components/categoriesBar/CategoriesBar.jsx
+++ frontend/src/components/categoriesBar/CategoriesBar.jsx
@@ -4,43 +4,54 @@ import axios from "axios";
 import { AppBar, FormControl, Link, Toolbar, Typography } from "@mui/material";
 
 const CategoriesBar = () => {
-  const [categoriesNameList, setCategoriesNameList] = useState([]);
+  const [categoriesNamesList, setCategoriesNamesList] = useState([]);
   const [categoriesIdsList, setCategoriesIdsList] = useState([]);
+  const [categoriesIconsList, setCategoriesIconsList] = useState([]);
 
   useEffect(() => {
     const getGategories = async () => {
       const provNameList = [];
       const ids = [];
+      const icons = [];
       const res = await axios.get("/categories");
 
       for (let i = 0; i < res.data.length; i++) {
         provNameList.push(res.data[i].name);
         ids.push(res.data[i]._id);
+        icons.push(res.data[i].icon);
       }
 
-      setCategoriesNameList(provNameList);
+      setCategoriesNamesList(provNameList);
       setCategoriesIdsList(ids);
+      setCategoriesIconsList(icons);
     };
     getGategories();
   }, []);
 
   const categoriesList = [];
 
-  for (let i = 0; i < categoriesNameList.length; i++) {
+  for (let i = 0; i < categoriesNamesList.length; i++) {
     categoriesList.push(
       <>
         <FormControl
           sx={{
             minWidth: "5%",
-            px:1,
+            px: 1,
             color: "#BDEFD8",
             backgroundColor: "#0E1428",
           }}
         >
-          <Link href={`/categories/${categoriesIdsList[i]}`} underline="none" sx={{ color: "#BDEFD8", backgroundColor: "#0E1428", mx:1 }}>
-              <div
-                dangerouslySetInnerHTML={{ __html: categoriesNameList[i] }}
-              />
+          <Link
+            href={`/categories/${categoriesIdsList[i]}`}
+            underline="none"
+            sx={{ color: "#BDEFD8", backgroundColor: "#0E1428", mx: 1 }}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html:
+                  "&#" + categoriesIconsList[i] + "; " + categoriesNamesList[i],
+              }}
+            />
           </Link>
         </FormControl>
       </>
@@ -49,7 +60,13 @@ const CategoriesBar = () => {
 
   return (
     <AppBar position="static">
-      <Toolbar sx={{ color: "#BDEFD8", backgroundColor: "#0E1428", fontFamily: "Lora, sans-serif" }}>
+      <Toolbar
+        sx={{
+          color: "#BDEFD8",
+          backgroundColor: "#0E1428",
+          fontFamily: "Lora, sans-serif",
+        }}
+      >
         <Typography variant="h6" sx={{ mr: 1 }}>
           Categorias
         </Typography>
